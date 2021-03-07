ff44b40def2aca2b4ac535bbf1207986e2ec9f95
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-07 23:30:35 +0900

    php class110

--- docker/html/php/1-index.php
+++ docker/html/php/1-index.php
@@ -1,44 +1,14 @@
 <?php
 
-$myArray = array("Rob","Kirstin","Tom","Ralph");
+$role = "admin";
+$position = "manager";
+// $position = "director";
 
-print_r($myArray); //print readable
 
-echo "<br><br>";
-
-echo $myArray[1];
-
-echo "<br><br>";
-
-$anotherArray[0] = "pizza";
-$anotherArray[1] = "chocolate";
-$anotherArray[5] = "coffee";
-
-print_r($anotherArray);
-
-echo "<br><br>";
-
-$anotherArray["myFavorite"] = "icecream";
-
-print_r($anotherArray);
-
-$thirdArray = array("Flance" => "French",
-                "USA" => "English",
-                "Germany" => "German");
-
-echo "<br><br>";
-
-print_r($thirdArray);
-echo "<br><br>";
-
-echo sizeof($thirdArray);
-
-unset($thirdArray["Flance"]);
-echo "<br><br>";
-
-echo sizeof($thirdArray);
-
-print_r($thirdArray);
-
+if($role == "admin" || $position == "director"){
+    echo "<p>管理画面に進んでください</p>";
+} else {
+    echo "<p>do not enter </p>";
+}
 
 ?>
