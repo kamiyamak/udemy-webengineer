f06ac4ef56fa4553d5ff755c4859feff725d9ac4
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-07 23:50:13 +0900

    php class111

--- docker/html/php/1-index.php
+++ docker/html/php/1-index.php
@@ -1,14 +1,27 @@
 <?php
 
-$role = "admin";
-$position = "manager";
-// $position = "director";
+for($i = 10;$i >= 0;$i--){
+    if ($i == 0){
+        echo $i;
+    } else {
+        echo $i.",";
+    }
+}
 
+echo "<br><br>";
 
-if($role == "admin" || $position == "director"){
-    echo "<p>管理画面に進んでください</p>";
-} else {
-    echo "<p>do not enter </p>";
+$family = array("Rob","Kirstin","Tom","Ralph");
+
+for($i = 0;$i < sizeof($family);$i++){
+    echo $family[$i].",";
+}
+
+echo "<br><br>";
+
+foreach($family as $key => $value){
+    // echo $key.": ",$value.".";
+    $family[$key] = $value." Percival";
+    echo $family[$key].", ";
 }
 
 ?>
