532c93cea42145455b020d3d27dc67dd26a29de3
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-10 09:36:57 +0900

    php class113

--- docker/html/php/1-index.php
+++ docker/html/php/1-index.php
@@ -1,20 +1,14 @@
 <?php
 
-$i = 0;
-while($i < 10){
-    echo $i."<br>";
-    $i++;
-}
-
-$family = array("Rob","Kirstin","Tom","Ralph");
-
-$i = 0;
-
-while($i < sizeof($family)){
-    echo $family[$i]." Percival, ";
-    $i++;
-}
+// print_r($_GET);
+echo "こんにちは,".$_GET['name']."さん";
 
 echo "<br><br>";
 
 ?>
+
+<p>名前を入力</p>
+<form>
+    <input type="text" name="name">
+    <input type="submit" value="送信する">
+</form>
