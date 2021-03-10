4b947e2da872375c27e0b5e3279589471be96888
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-10 09:41:58 +0900

    php class114

--- docker/html/php/1-index.php
+++ docker/html/php/1-index.php
@@ -1,14 +1,18 @@
 <?php
 
+if($_POST){
+    echo "こんにちは,".$_POST['name']."さん";
+} else {
+
+}
 // print_r($_GET);
-echo "こんにちは,".$_GET['name']."さん";
 
 echo "<br><br>";
 
 ?>
 
 <p>名前を入力</p>
-<form>
+<form method="post">
     <input type="text" name="name">
     <input type="submit" value="送信する">
 </form>
