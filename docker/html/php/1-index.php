025480a12f154019bfcfa1d63ed6f0682968ad80
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-10 09:28:57 +0900

    php class112

--- docker/html/php/1-index.php
+++ docker/html/php/1-index.php
@@ -1,27 +1,20 @@
 <?php
 
-for($i = 10;$i >= 0;$i--){
-    if ($i == 0){
-        echo $i;
-    } else {
-        echo $i.",";
-    }
+$i = 0;
+while($i < 10){
+    echo $i."<br>";
+    $i++;
 }
-
-echo "<br><br>";
 
 $family = array("Rob","Kirstin","Tom","Ralph");
 
-for($i = 0;$i < sizeof($family);$i++){
-    echo $family[$i].",";
+$i = 0;
+
+while($i < sizeof($family)){
+    echo $family[$i]." Percival, ";
+    $i++;
 }
 
 echo "<br><br>";
-
-foreach($family as $key => $value){
-    // echo $key.": ",$value.".";
-    $family[$key] = $value." Percival";
-    echo $family[$key].", ";
-}
 
 ?>
