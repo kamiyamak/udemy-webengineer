f89088e41baa1c6a7daf8278472be3ad3f6f8b89
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-13 12:31:18 +0900

    mysql class126

--- docker/html/mysql/mysql.php
+++ docker/html/mysql/mysql.php
@@ -5,25 +5,18 @@
         die("失敗しました");
     }
 
-    // $query = "insert into `users` (`email`,`password`) values ('Kirsten@gmail.com','thereisnopassword')";
-
-    // if($result = mysqli_query($link,$query)) {
-    //     echo "Insert成功しました";
-    // }
-
-    $query = "update `users` SET password='changedpassword' where email='Rob@gmail.com' LIMIT 1";
-    if($result = mysqli_query($link,$query)) {
-        echo "update成功しました";
-        echo "<br>";
-    }
-    $query = "SELECT * from users";
+    // $query = "SELECT * from users WHERE id = 1";
+    $query = "SELECT * from users WHERE name LIKE 'K%'";
     if($result = mysqli_query($link,$query)) {
         // echo "Insert成功しました";
     }
-    $row = mysqli_fetch_array($result);
+    while($row = mysqli_fetch_array($result)){
+        print_r($row);
+    }
+
     // print_r($row);
 
-    echo "あなたのメールアドレスは".$row['email']."、パスワードは".$row['password']."です。";
-    // echo "<p>あなたのメールアドレスは".$row['1']."、パスワードは".$row['2']."です。";
-    // echo "<p>あなたのIDは".$row['id']."です。";
+    // echo "あなたのメールアドレスは".$row['email']."、パスワードは".$row['password']."です。";
+    // // echo "<p>あなたのメールアドレスは".$row['1']."、パスワードは".$row['2']."です。";
+    // // echo "<p>あなたのIDは".$row['id']."です。";
 ?>
