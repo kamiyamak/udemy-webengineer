faf9f3390a8e1fee0a87373f7812ec9d0f4d9bd0
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-13 12:20:55 +0900

    mysql class124

--- docker/html/mysql/mysql.php
+++ docker/html/mysql/mysql.php
@@ -5,15 +5,25 @@
         die("失敗しました");
     }
 
-    $query = "SELECT * from users";
+    // $query = "insert into `users` (`email`,`password`) values ('Kirsten@gmail.com','thereisnopassword')";
 
+    // if($result = mysqli_query($link,$query)) {
+    //     echo "Insert成功しました";
+    // }
+
+    $query = "update `users` SET password='changedpassword' where email='Rob@gmail.com' LIMIT 1";
     if($result = mysqli_query($link,$query)) {
-        // echo "成功しました";
+        echo "update成功しました";
+        echo "<br>";
+    }
+    $query = "SELECT * from users";
+    if($result = mysqli_query($link,$query)) {
+        // echo "Insert成功しました";
     }
     $row = mysqli_fetch_array($result);
     // print_r($row);
 
     echo "あなたのメールアドレスは".$row['email']."、パスワードは".$row['password']."です。";
-    echo "<p>あなたのメールアドレスは".$row['1']."、パスワードは".$row['2']."です。";
-    echo "<p>あなたのIDは".$row['id']."です。";
+    // echo "<p>あなたのメールアドレスは".$row['1']."、パスワードは".$row['2']."です。";
+    // echo "<p>あなたのIDは".$row['id']."です。";
 ?>
