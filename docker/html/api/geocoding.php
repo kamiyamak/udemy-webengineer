7c15d5fbd3e63458f1090078408a6ad653f58474
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-21 16:34:23 +0900

    class 147

---
+++ docker/html/api/geocoding.php
@@ -0,0 +1,5 @@
+<?php
+
+echo file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBRDkaguj_B64-DhD07atnDHYL0SaJT0eQ");
+
+?>
