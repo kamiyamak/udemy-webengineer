cdddd87769187d9f26bc598a34be2b4085ac5682
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-27 16:06:19 +0900

    python class166

--- 13-python/hello.py
+++ 13-python/hello.py
@@ -4,57 +4,89 @@ print 'Content-type: text/html'
 print ''
 # print 'Hello Python!!'
 
-age = 35
+# age = 35
 
-print age
+# print age
 
-print ''
+# print ''
 
-pi = 3.14
+# pi = 3.14
 
-print pi
+# print pi
 
-name = 'Rob'
+# name = 'Rob'
 
-print name 
+# print name 
 
-print age/pi
+# print age/pi
 
-number = "5"
+# number = "5"
 
-print number * age
+# print number * age
 
-print '<br>'
+# print '<br>'
 
-print int(number) * 5
+# print int(number) * 5
 
-print int(number) * age
+# print int(number) * age
 
-print int(number) * pi
+# print int(number) * pi
 
-str = "my name is rob"
+# str = "my name is rob"
 
-print str[0]
-print str[0:6]
-print str[5]
+# print str[0]
+# print str[0:6]
+# print str[5]
 
-myList = ["apple","banana","orange"]
-print myList
-print myList[1]
-print myList[1:4]
+# myList = ["apple","banana","orange"]
+# print myList
+# print myList[1]
+# print myList[1:4]
 
-myTuple = (1,2,3,4)
-print myTuple
-# myTuple[1] = 5
+# myTuple = (1,2,3,4)
 # print myTuple
+# # myTuple[1] = 5
+# # print myTuple
 
-dict = {}
-dict["dad"] = "rob"
-dict["mum"] = "kirstin"
-dict[1] = "tom"
-dict[2] = "ralph"
+# dict = {}
+# dict["dad"] = "rob"
+# dict["mum"] = "kirstin"
+# dict[1] = "tom"
+# dict[2] = "ralph"
 
-print dict
-print dict["mum"]
-print dict.keys()
-print dict.values()
+# print dict
+# print dict["mum"]
+# print dict.keys()
+# print dict.values()
+
+for i in range(5,11):
+    print i
+    
+print "rob"
+
+print "<br>"
+
+list = ["pizza","chocolate","icecream"]
+
+# i like eating ***
+
+for food in list:
+    print "I like eating " + food + "."
+    print "<br>"
+
+x = 0
+while x <= 10:
+    print x
+    x = x + 1
+    # x += 1
+
+print "<br>"
+
+ages = {}
+ages["Rob"] = 35
+ages["Kirstin"] = 36
+ages["Tom"] = 5
+ages["raph"] = 1
+
+for age in ages:
+    print age + " is " + str(ages[age]) + "."
