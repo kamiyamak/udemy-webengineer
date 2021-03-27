31f6b834452ce1ccf94ff9fe80aed6c59cf8540c
kamiyamak <kamiyamak@istyle.co.jp>
2021-03-27 14:48:04 +0900

    python class164

--- 13-python/hello.py
+++ 13-python/hello.py
@@ -2,4 +2,59 @@
 
 print 'Content-type: text/html'
 print ''
-print 'Hello Python!!'
+# print 'Hello Python!!'
+
+age = 35
+
+print age
+
+print ''
+
+pi = 3.14
+
+print pi
+
+name = 'Rob'
+
+print name 
+
+print age/pi
+
+number = "5"
+
+print number * age
+
+print '<br>'
+
+print int(number) * 5
+
+print int(number) * age
+
+print int(number) * pi
+
+str = "my name is rob"
+
+print str[0]
+print str[0:6]
+print str[5]
+
+myList = ["apple","banana","orange"]
+print myList
+print myList[1]
+print myList[1:4]
+
+myTuple = (1,2,3,4)
+print myTuple
+# myTuple[1] = 5
+# print myTuple
+
+dict = {}
+dict["dad"] = "rob"
+dict["mum"] = "kirstin"
+dict[1] = "tom"
+dict[2] = "ralph"
+
+print dict
+print dict["mum"]
+print dict.keys()
+print dict.values()
